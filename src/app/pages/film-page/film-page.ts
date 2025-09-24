391b625d652fded44a81e24f635a7bb21636485c
Igor <96075714+hammer6161@users.noreply.github.com>
2025-09-25 00:47:38 +0300

    Add files via upload

---
+++ src/app/pages/film-page/film-page.ts
@@ -0,0 +1,42 @@
+import { Component, OnInit } from '@angular/core';
+import { FILMS } from '../../constants/film.constants';
+import { Film } from '../../models/film.models';
+import { ActivatedRoute, Router } from '@angular/router';
+import { Films } from '../../services/films';
+
+@Component({
+  selector: 'app-film-page',
+  standalone: false,
+  templateUrl: './film-page.html',
+  styleUrl: './film-page.css'
+})
+export class FilmPage implements OnInit {
+	
+	public films: Film[] = []
+	public film!: Film
+
+	constructor(
+		private _route: ActivatedRoute,
+		private _router: Router,
+		private _filmsService: Films
+	) {}
+
+	ngOnInit():void {
+		const id = this._route.snapshot.paramMap.get('id')
+
+		this.films = this._filmsService.getFullFilms
+		
+		this._initFilm(Number(id))
+	}
+
+	private _initFilm(id: number) {
+		const findFilm = this.films.find(film => film.id === id)
+		
+		if(findFilm) {
+			this.film = findFilm
+		}else {
+			this._router.navigate(['/'])
+		}
+	}
+	
+}
